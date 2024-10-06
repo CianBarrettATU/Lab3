91b038c29063636aa20c0d65e7fb2c40cd45955d
CianBarrettATU <127865337+CianBarrettATU@users.noreply.github.com>
2024-10-06 19:36:53 +0100

    Update Content.js comments

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -4,6 +4,7 @@ const Content = () => {
   return (
     <div>
       <h1>Hello World!</h1>
+      //displays current time and converts to a string
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
